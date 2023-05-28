c1128b8b2f2101aa7c8e881305ff563381acc16a
AramHovhannisyan <8hovhannisyanaram@gmail.com>
2023-05-28 13:03:50 +0400

    small fixes for refreshToken update

--- src/services/tokenService.ts
+++ src/services/tokenService.ts
@@ -6,16 +6,20 @@ import UserDto from '../dtos/UserDto';
 const generateTokens = async (payload: UserDto) => {
   const { username, email } = payload;
   const accessToken = await jwt.sign({ username, email }, config.jwt.secretAccess, { expiresIn: '15m' });
-  const refreshToken = await jwt.sign({ username, email }, config.jwt.secretRefresh, { expiresIn: '15m' });
+  const refreshToken = await jwt.sign({ username, email }, config.jwt.secretRefresh, { expiresIn: '15d' });
 
   return { accessToken, refreshToken };
 };
 
 const saveToDb = async (user: UserDto, refreshToken: string) => {
-  const isTokenSaved = await Token.create({ user: user.id, refreshToken });
+  const oldToken = await Token.findOne({ user: user.id});
 
-  console.log('isTokenSaved:', isTokenSaved);
-  
+  if (oldToken) {
+    oldToken.refreshToken = refreshToken;
+    return oldToken.save(); 
+  }
+
+  return await Token.create({ user: user.id, refreshToken });
 };
 
 export { generateTokens, saveToDb };
