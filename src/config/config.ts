eb43d3871a8ca91e3ae27aceef1afa0802fde4c5
aramHovhannisyan <8hovhannisyanaram@gmail.com>
2024-06-27 15:46:29 +0400

    Sync env variables with docker from .env file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,12 @@
 import dotenv from 'dotenv';
+dotenv.config({ path: `.env` });
 
 const NODE_ENV = process.env.NODE_ENV || 'prod';
-dotenv.config({ path: `.env.${NODE_ENV}` });
-
 const SERVER_PORT = process.env.SERVER_PORT || 3003;
 const JWT_SECRET_ACCESS = process.env.JWT_SECRET_ACCESS || 'MY-SEC';
 const JWT_SECRET_REFRESH = process.env.JWT_SECRET_ACCESS || 'MY-SEC2';
 const MONGO_HOST = process.env.MONGO_HOST;
-const MONGO_PORT = process.env.MONGO_PORT;
+const MONGO_PORT = process.env.MONGO_LOCAL_PORT;
 const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 
